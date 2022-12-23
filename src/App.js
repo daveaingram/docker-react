28245e9a60d0fafc25f3d62976d41d7c240bee35
Dave Ingram <dave@davei.xyz>
2022-12-23 16:18:03 -0500

    changing hi to hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Well hi there!!
+          Well hello there!!
         </p>
         <a
           className="App-link"
